91304b770f52b2dece162fe6fac735ffa22c5cc7
Pamelia Lettie <plettie@il-j2.21-school.ru>
2019-06-07 22:18:41 +0300

    cdelav no ne vse

--- filit.c
+++ filit.c
@@ -14,25 +14,31 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-char		*ft_strchr(const char *s, int c)
-{
-	char	*str;
-	int		i;
-	char	k;
+#define onError(head){\
+        free(head);\
+        head = NULL;\
+        return(0);\
+}\
 
-	k = (char)c;
+char		*ft_strchr(char *s, char resh)
+{
+	int		i;
+    int count;
+
+    count = 0;
 	i = 0;
-	while (s[i] != k)
+	while (s[i] != resh)
 	{
-		if (!s[i])
+		if (!s[i] || (s[i] != '.' && s[i] != '\n') || count > 3)
 			return (0);
+        if (s[i] == '\n')
+            count++;
 		i++;
 	}
-	str = (char*)s;
-	return (&str[i]);
+	return (&s[i]);
 }
 
-char is_valid(int fd)
+char isValid(int fd)
 {
     char *c = malloc(21);
     char *head;
@@ -44,24 +50,24 @@ char is_valid(int fd)
     time = 0;
     while ((kol = read(fd, c, 21)) > 0 && ++time < 27)
     {
-        if (kol != 21 || c[4] != '\n' || !((c[9] | c[14] | c[19] | c[20]) == c[4]))
+        if (kol < 20 || c[4] != '\n' || (c[9] | c[14] | c[19]) != c[4])
             return (0);
-        count = 0;
-        while (*c && count < 5)
+        count = kol;
+        while (*c && count < 26)
         {
             c = ft_strchr(c, '#');
             if (!c)
                 break;
             count++;
             if (*(c - 1) != '#' && *(c + 1) != '#' && *(c - 5) != '#' && *(c + 5) != '#')
-                return (0);
+                onError(head)
             c++;
         }
-        if (count != 4)
-            return (0);
+        if (count != 25 - (kol == 20) ? (1) : (0))
+            onError(head)
         c = head;
     }
-    if (kol == -1 || time > 26)
+    if (kol == -1 || time > 26 || count != 24)
         return (0);
     return (1);
         
@@ -77,8 +83,8 @@ int main(int ac, char **av)
         write(1, "usage", 5);
         return (0);
     }
-    fd = open("test.txt", O_RDONLY);
-    if (is_valid(fd))
+    fd = open(av[1], O_RDONLY);
+    if (isValid(fd))
         write(1, "harosh", 6);
     else
         write(1, "sore ne valid", 13);
