6a874e954e80f7689598a15d09ef2c26b8749328
Caryl Ellis <caellis@mi-t2.21-school.ru>
2019-06-07 15:10:47 +0300

    tester.sh BIG PIMPIN (now checks errors and 0/2 arguments)

--- tester.sh
+++ tester.sh
@@ -6,7 +6,7 @@
 #    By: caellis <caellis@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/07 14:05:14 by caellis           #+#    #+#              #
-#    Updated: 2019/06/07 14:07:45 by caellis          ###   ########.fr        #
+#    Updated: 2019/06/07 15:09:53 by caellis          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,9 +15,52 @@
 # Запусти скрипт, и он сгенерирует карту и запустит филлит вместе с ней 
 # (а также засечет и покажет время)
 
+# COLORS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0;m'
+
+# MAKING
 make -C libft/ re ; make clean
 make re ; make clean
-python3 development_/rand_test.py > curr_test
-echo "THIS IS YOUR TEST\n==>\n" && cat curr_test
-echo "\nand THIS IS YOU FILLIT OUTPUT\n==>\n" && time ./fillit curr_test
+
+# Input test-files location
+ERROR=development_/tetriminox_tester/input/error/*
+VALID=development_/tetriminox_tester/input/valid/*
+
+# TESTING 0 and 2 arguments
+echo "\nTHIS IS TO CHECK 0 & 2 inputs\n==>\n"
+echo "0 arguments:\n"$RED
+./fillit
+echo $NC"\n2 arguments:\n"$RED
+./fillit development_/tetriminox_tester/input/valid/valid_00 development_/tetriminox_tester/input/valid/valid_01
+echo $NC
+read -n 1 -s -r -p "Press any key to continue"
+
+# TESTING ERRORS
+echo "\nSTARTING ERROR TESTING\n==>\n"
+for n in $ERROR;
+do
+	echo "Error test $RED[$i]$NC:\n"
+	echo "$BLUE[Input]$NC\n"
+	cat $n
+	echo "\n$BLUE[Output]$NC\n"$RED
+	time ./fillit $n
+	(( i++ ))
+	echo $NC
+	read -n 1 -s -r -p "Press any key to continue"
+done
+
+# TESTING OK INPUT - works or not (not right or wrong)
+for ((i = 0; i < 3; i++))
+do 
+	python3 development_/rand_test.py > curr_test ;
+	echo "\nTHIS IS $BLUE CURRENT VALID TEST $NC \n==>\n" && cat curr_test
+	echo "\nand THIS IS $BLUE YOUR FILLIT OUTPUT $NC \n==>\n$RED" && time ./fillit curr_test
+	echo $NC
+	read -n 1 -s -r -p "Press any key to continue"
+done
+
 rm -rf curr_test
