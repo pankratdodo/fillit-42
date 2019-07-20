c161e20637fb9b6914c6cbd17cb9657b12279312
caellis <serkhar@gmail.com>
2019-07-21 02:06:49 +0300

    solve_it with comments

--- solve_it.c
+++ solve_it.c
@@ -6,7 +6,7 @@
 /*   By: caellis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 17:41:22 by caellis           #+#    #+#             */
-/*   Updated: 2019/07/20 19:51:45 by caellis          ###   ########.fr       */
+/*   Updated: 2019/07/21 02:06:19 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,20 @@ int		insert_figure(t_cell *map, t_tetris *fig, int pos, int side)
 	int			i;
 	int			x_ctl;
 	int			y_ctl;
+	int			flag;
 	int			pos_to_fill[4];
 
+	flag = 0;
 	i = 0;
 	while (i < 4)
 	{
 		x_ctl = map[pos].x + fig->shape[i].x;
 		y_ctl = map[pos].y + fig->shape[i].y;
-		if (x_ctl < 0 || x_ctl >= side || y_ctl < 0 || y_ctl >= side)
-			return (0);
+		if (x_ctl < 0 || x_ctl >= side || y_ctl < 0 || y_ctl >= side)	// выходит за пределы карты (О - здесь можно также делать проверку "может ли вообще поместиться"
+		{
+			flag = 1;
+			break; // было return(0) - стало break + flag, также на строчке 76;
+		}
 		if (map[pos + fig->shape[i].x * side + fig->shape[i].y].ind != '.')
 			return (0);
 		else
@@ -68,7 +73,7 @@ int		insert_figure(t_cell *map, t_tetris *fig, int pos, int side)
 		i++;
 	}
 	i = 0;
-	while (i < 4)
+	while (!flag && i < 4)
 	{
 		map[pos_to_fill[i]].ind = fig->index;
 		i++;
@@ -102,6 +107,8 @@ t_cell		*solve_it(t_list *solutions, t_tetris *figures, int side)
 
 	pos = 0;
 	MALL_CHECK(map = map_init(side));
+	// нужно вставить сюда условия перезапуска cracker'а, чтобы не первое попавшееся решение брать (тем более с расширением карты, которое было не нужно)
+	// кстати, в инсерте я одинаково реагирую как на выход за пределы ряда, так и на невозможность вставки
 	if (!cracker(map, figures, solutions, pos, side))
 	{
 		free(map);
